9d4f0073f1df33c0746fd89a96b41f76314defcf
mluan23 <luanmrun20@gmail.com>
2025-11-01 13:46:27 -0400

    fe

---
+++ src/backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json()) // allow for JSON
+
+let users = [
+    { id: 1, name: 'Alice'}
+]
+
+// root
+app.get('/', (req, res) => {
+    res.send('test')
+})
+
+// get all users
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+// array of posts -> name + image link + description + id
+// adding new user, user o
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+app.listen(port, () => {
+    console.log('server started')
+})
